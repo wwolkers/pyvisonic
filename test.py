5159fc255b554984eecb8c462724098560fa8919
David <mickey@disney.com>
2018-05-19 14:22:11 +0100

    This version connects via Ethernet TCP/IP bridge to the panels RS232. I use a wireless USR-TCP232-E

--- test.py
+++ test.py
@@ -7,12 +7,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 testloop = asyncio.get_event_loop()
 
-conn = pyvisonic.create_visonic_connection(
+#conn = pyvisonic.create_visonic_connection(
+#    protocol=pyvisonic.VisonicProtocol,
+#    port='/dev/ttyUSB1',
+#    loop=testloop)
+conn = pyvisonic.create_tcpvisonic_connection(
     protocol=pyvisonic.VisonicProtocol,
-    port='/dev/ttyUSB1',
+    address='192.168.0.8',
+    port=20024,
     loop=testloop)
 testloop.create_task(conn)
-protocol = testloop.run_until_complete(conn)
+#protocol = testloop.run_until_complete(conn)
 #protocol.send_command_ack(packet = VMSG_ENROLL)
 # loop.run_until_complete(m.transport)
 try:
